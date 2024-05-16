ac00ed3414fc26706792e3d73d346b5fd01a0b14
Samandar70 <165376960+Samandar70@users.noreply.github.com>
2024-05-16 13:22:19 -0400

    Add files via upload

---
+++ Samandar4.js
@@ -0,0 +1,147 @@
+
+// let day = +prompt("Sana kirit");
+// if (day ==1) {
+//     alert ("Yakshanba")
+// } else if (day ==2) {
+//     alert ("Dushanba")
+// } else if (day ==3) {
+//     alert ("Seshanba")
+// } else if (day ==4) {
+//     alert ("Chorshanba")
+// } else if (day ==5) {
+//     alert ("Payshanba")
+// } else if (day ==6) {
+//     alert ("Juma")
+// } else if (day ==7) {
+//     alert ("Shanba")
+// } else {
+//     alert("Bunaqa Sana yuq")
+// }
+
+// let son = +prompt("Son kirit");
+// if (son ==1) {
+//     alert ("yomon")
+// } else if (son ==2) {
+//     alert ("qoniqarsiz")
+// } else if (son ==3) {
+//     alert ("qoniqarli")
+// } else if (son ==4) {
+//     alert ("yahshi")
+// } else if (son ==5) {
+//     alert ("a'lo")
+// }  else {
+//     alert("Bunaqa Son yuq")
+// }
+
+// let month = +prompt("Oy");
+// if (month == 1) {
+//     alert("Qish");
+// } else if (month == 2) {
+//     alert("Qish");
+// } else if (month == 3) {
+//     alert("Bahor");
+// } else if (month == 4) {
+//     alert("Bahor");
+// } else if (month == 5) {
+//     alert("Bahor");
+// } else if (month == 6) {
+//     alert("Yoz");
+// } else if (month == 7) {
+//     alert("Yoz");
+// } else if (month == 8) {
+//     alert("Yoz");
+// } else if (month == 9) {
+//     alert("Kuz");
+// } else if (month == 10) {
+//     alert("Kuz");
+// } else if (month == 11) {
+//     alert("Kuz");
+// } else if (month == 12) {
+//     alert("Qish");
+// }
+
+// let OY = +prompt("Oy");
+// if (OY == 1) {
+//     alert("Yanvar");
+// } else if (OY == 2) {
+//     alert("Fevral");
+// } else if (OY == 3) {
+//     alert("Mart");
+// } else if (OY == 4) {
+//     alert("Aprel");
+// } else if (OY == 5) {
+//     alert("May");
+// } else if (OY == 6) {
+//     alert("Iyun");
+// } else if (OY == 7) {
+//     alert("Iyul");
+// } else if (OY == 8) {
+//     alert("Avgust");
+// } else if (OY == 9) {
+//     alert("Sentiyabr");
+// } else if (OY == 10) {
+//     alert("Oktiyabr");
+// } else if (OY == 11) {
+//     alert("Noyabr");
+// } else if (OY == 12) {
+//     alert("Dekabr");
+// } else {
+//     alert("bunaka oy yuq")
+// }
+
+// let taraf = prompt("Qaysi tarafga");
+// if (taraf == "w") {
+//     alert("Oldinga");
+// } else if (taraf == "s") {
+//     alert("Orqaga");
+// } else if (taraf == "a") {
+//     alert("Chapga");
+// } else if (taraf == "d") {
+//     alert("O'nga");
+// } else {
+//     alert("Bunaqa belgi yuq")
+// }
+
+// let butun  = +prompt("butun kirit");
+// if (butun  == 1) {
+//     alert ("Qoshish")
+// } else if (butun  == 2 ) {
+//     alert ("Airish")
+// } else if (butun  == 3 ) {
+//     alert ("Bolish")
+// } else if (butun  == 4 ) {
+//     alert ("Kopaytirish")
+// } else {
+//     alert("Bunaqa butun  yuq")
+// }
+
+// let Uzunlik = +prompt("Uzunlik kirit");
+// if (Uzunlik ==1) {
+//     alert ("desimetr")
+// } else if (Uzunlik ==2) {
+//     alert ("kilometr")
+// } else if (Uzunlik ==3) {
+//     alert ("metr")
+// } else if (Uzunlik ==4) {
+//     alert ("milimetr")
+// } else if (Uzunlik ==5) {
+//     alert ("Santimetr")
+// }  else {
+//     alert("Bunaqa Uzunlik yuq")
+// }
+
+let Ogirlik = +prompt("Sana kirit");
+if (Ogirlik ==1) {
+    alert ("Kilogram")
+} else if (Ogirlik ==2) {
+    alert ("milligram")
+} else if (Ogirlik ==3) {
+    alert ("gram")
+} else if (Ogirlik ==4) {
+    alert ("tonna")
+} else if (Ogirlik ==5) {
+    alert ("senter")
+}  else {
+    alert("Bunaqa Ogirlik yuq")
+}
+
